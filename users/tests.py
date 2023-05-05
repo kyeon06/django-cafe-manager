5c2ba7592eb9b679bf278ddf0c73b137ace7f5a2
kyeon06 <jkyeon06@gmail.com>
2023-05-05 21:24:49 +0900

    :white_check_mark: [test] user 회원가입 테스트

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+class UserTestCase(APITestCase):
+    def setUp(self):
+        pass
+
+    # 회원가입 test
+    def test_register_user(self):
+        url = '/user/register/'
+        data = {'phone_number' : '010-1234-5678', 'password' : 'testpass'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
