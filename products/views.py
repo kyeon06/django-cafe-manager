a7a11a775bf106145703a28cf69e2b1d3dbbd61c
kyeon06 <jkyeon06@gmail.com>
2023-05-07 19:54:27 +0900

    :sparkles: [feat] Product 수정 기능 구현

--- products/views.py
+++ products/views.py
@@ -37,3 +37,21 @@ class ProductAPIView(APIView):
             return CustomResponse(data=serializer.data, message="상품이 등록되었습니다." ,status=status.HTTP_201_CREATED)
        
         return CustomResponse(data=serializer.errors, message="상품 정보를 다시 확인해주세요." ,status=status.HTTP_400_BAD_REQUEST)
+    
+    # 상품 수정
+    def put(self, request, **kwags):
+        if kwags.get('pk') is None:
+            return CustomResponse(data=None, message="잘못된 요청입니다.", status=status.HTTP_400_BAD_REQUEST)
+        else:
+
+            try:
+                product_id = kwags.get('pk')
+                product_object = Product.objects.get(id=product_id)
+            except Product.DoesNotExist:
+                return CustomResponse(message="상품 정보가 존재하지 않습니다.",status=status.HTTP_404_NOT_FOUND)
+            
+            serializer = ProductSerializer(product_object, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return CustomResponse(data=serializer.data, status=status.HTTP_200_OK)
+            return CustomResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
